f6af6dd408624da3fdba68e64324db0d7bbd7b72
FehmiYousfi <fehmi.yousfi@etudiant-enit.utm.tn>
2023-08-07 01:08:19 +0100

    no more errors in the Driver.c file some warning due to types and etc some errores in building the module because of the definition of the Vendors ID and Product ID , 01:07 07_08_2023

---
+++ .modules.order.cmd
@@ -0,0 +1,1 @@
+cmd_/home/virtual/Desktop/Custom_Driver/USB_Driver_Controller/modules.order := {   echo /home/virtual/Desktop/Custom_Driver/USB_Driver_Controller/Driver.ko; :; } | awk '!x[$$0]++' - > /home/virtual/Desktop/Custom_Driver/USB_Driver_Controller/modules.order
--- Driver.c
+++ Driver.c
@@ -10,34 +10,36 @@
 
 struct usb_class_driver usb_cd;
 
-static int usb_drv_probe(struct usb_interface *interface, const struct usb_device_id *id)
+static int usb_drv_probe( struct usb_interface *interface, const struct usb_device_id *id)
 {
 	struct usb_host_interface *interface_desc;
 	struct usb_end_descriptor * endpoint;
 	int ret;
 
-	interface_desc = interface->cur_altersetting;
+	interface_desc = interface->cur_altsetting;
 	printk(KERN_INFO "USBinfo %d now probed: (%04X: %04)\n", interface_desc->desc.bInterfaceNumber, id->idVendor, id->idProduct);
 	printk(KERN_INFO "ID->bNumberEndpoints : %02X\n",interface_desc->desc.bNumEndpoints);
-	printk(KERN_INFO "ID-> bIntrefaceClass: %02X\n", interface_desc.desc.bInterfaceClass);
+	printk(KERN_INFO "ID-> bIntrefaceClass: %02X\n", interface_desc->desc.bInterfaceClass);
 	ret = usb_register_dev(interface,&usb_cd);
 	if(ret)
 	{
 		printk(KERN_INFO "Not able yo get the minor number..\n");
+		return(1);
 	}
 	else
 	{
 		printk(KERN_INFO "Minor Number = %d\n", interface->minor);
+		return(0);
 	}
 }
 
-static struct usb_device_disconnect(struct usb_interface *interface)
+static void  usb_drv_disconnect (struct usb_interface *interface)
 {
-	printk(KERN_INFO "Disconnect and Release the Minor Number %d\n", intreface->minor);
+	printk(KERN_INFO "Disconnect and Release the Minor Number %d\n", interface->minor);
 	usb_deregister_dev(interface,&usb_cd);
 }
 
-static struct usd_device_id usb_drv_table[] = {
+static struct usb_device_id usb_drv_table[] = {
 
 	{USB_DEVICE(USB_VENDOR_ID,USB_PRODUCT_ID)}
 
@@ -54,21 +56,22 @@ static struct usb_driver usb_drv_struct = {
 
 static int __init usb_test_init(void)
 {
-	printk(KERN_INFO "Rgister the usb driver with the usb subsystem\n");
+	printk(KERN_INFO "Registered the usb driver with the usb subsystem\n");
 	usb_register(&usb_drv_struct);
+	return(0);
 }
 
 static void __exit usb_test_exit(void)
 {
-	printk(KERN_INFO "Dergister the usb driver From the usb subsystem\n");
+	printk(KERN_INFO "Dergistered the usb driver From the usb subsystem\n");
 	usb_deregister(&usb_drv_struct);
 }
 module_init(usb_test_init);
 module_exit(usb_test_exit);
 
-MODULE_LICENCE("GPL");
+MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Luceor Labs <fehmi.yousfi@Luceor.com>");
-MODULE_DISCRIPTION("USB_test_Driver")
+MODULE_DESCRIPTION("USB_test_Driver");
 
 
 
